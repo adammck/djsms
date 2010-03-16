bf6c1c53bcd6ad3821c105c65b43af3331a82528
adammck <adam.mckaig@gmail.com>
2010-03-15 21:24:01 -0400

    added some models to keep track of messages.

---
+++ sms/models.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# vim: et ts=4 sw=4
+
+
+from django.db import models
+
+
+class Backend(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
+class Connection(models.Model):
+    backend  = models.ForeignKey(Backend)
+    identity = models.CharField(max_length=100)
+
+
+class IncomingMessage(models.Model):
+    connection  = models.ForeignKey(Connection)
+    received_at = models.DateTimeField()
+    text        = models.TextField()
+
+
+class OutgoingMessage(models.Model):
+    connection = models.ForeignKey(Connection)
+    sent_at    = models.DateTimeField()
+    text       = models.TextField()
