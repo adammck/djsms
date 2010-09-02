de027ac039bd00f0a63f43feb1163379468dc21e
adammck <adam.mckaig@gmail.com>
2010-09-02 19:20:27 -0400

    made runrouter 'interval' an option, to match runbackend.

--- sms/management/commands/runrouter.py
+++ sms/management/commands/runrouter.py
@@ -12,19 +12,23 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option(
             "--reload", action="store_true", dest="use_reloader",
-            help="Tells Django to use the auto-reloader."),)
+            help="Tells Django to use the auto-reloader."),
+
+        make_option(
+            "--interval", default=10, dest="interval", type="float",
+            help="Specifies the how often to check for new messages"))
 
     help = "Starts the SMS router."
-    args = '[optional polling interval]'
 
     # model validation is called explicitly each time the router is
     # reloaded, so it doesn't need to be performed on startup
     requires_model_validation = False
 
 
-    def handle(self, interval=2, *args, **options):
+    def handle(self, *args, **options):
         quit_command = (sys.platform == "win32") and "CTRL-BREAK" or "CONTROL-C"
         use_reloader = options.get("use_reloader", False)
+        interval = options.get("interval")
 
         def inner_run():
             from ...models import IncomingMessage
